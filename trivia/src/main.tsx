dfa0b92998ae9a1ffcb8c5c90a98a3377122fe6b
Tamanna <tamannakhanum@hotmail.co.uk>
2024-03-12 17:34:56 +0000

    "Created router to direct app to page components."

--- trivia/src/main.tsx
+++ trivia/src/main.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+//? Styles
 import './styles/main.scss'
+
+//? Components
+import App from './App.tsx'
+import Home from './components/Home.tsx'
+import SelectQuiz from './components/SelectQuiz.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/select-quiz',
+        element: <SelectQuiz />
+      }
+    ]
+  }
+])
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
