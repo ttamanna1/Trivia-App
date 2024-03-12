611db10b6ab4e933563eaa5d874374c65712e464
Tamanna <tamannakhanum@hotmail.co.uk>
2024-03-12 16:31:34 +0000

    "Added animated background created by agathaco from codepen- adjusted to my desired styling."

--- trivia/src/App.tsx
+++ trivia/src/App.tsx
@@ -7,11 +7,16 @@ function App() {
     <>
       <div className="container" style={ { backgroundImage: `url(${background})` } }></div>
       <div className='content'>
-        <h1>Trivia</h1>
+        <div className="wrapper">
+          <div className="heading"> 
+            <span className="char1" data-heading="The">The</span> 
+            <span className="char2" data-heading="Ultimate">Ultimate</span> 
+            <span className="char3" data-heading="Trivia">Trivia</span> 
+            <span className="char4" data-heading="Extravaganza">Extravaganza</span> 
+            <span className="char5" data-heading="!">!</span>    
+          </div>
+        </div>
       </div>
-      
-      
-      
     </>
   )
 }
