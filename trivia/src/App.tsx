c27871cdb13365f9c84b03b86369a5781114f549
Tamanna <tamannakhanum@hotmail.co.uk>
2024-03-12 17:01:09 +0000

    "Added a moving button."

--- trivia/src/App.tsx
+++ trivia/src/App.tsx
@@ -1,6 +1,28 @@
-
+import { useEffect } from "react"
 
 function App() {
+  useEffect(() => {
+    const button: HTMLElement | null = document.getElementById('movingButton')
+
+    function moveButton(): void {
+      if (!button) return
+
+      const maxX: number = window.innerWidth - (button.offsetWidth || 0)
+      const maxY: number = window.innerHeight - (button.offsetHeight || 0)
+
+      const randomX: number = Math.floor(Math.random() * maxX)
+      const randomY: number = Math.floor(Math.random() * maxY)
+
+      if (button.style) {
+        button.style.left = randomX + 'px'
+        button.style.top = randomY + 'px'
+      }
+    }
+
+    const intervalId = setInterval(moveButton, 3000)
+
+    return () => clearInterval(intervalId)
+  }, [])
 
 
   return (
@@ -16,6 +38,7 @@ function App() {
             <span className="char5" data-heading="!">!</span>    
           </div>
         </div>
+        <button id="movingButton">Enter!</button>
       </div>
     </>
   )
