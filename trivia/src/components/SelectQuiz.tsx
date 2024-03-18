0bb205a5ad6e80ba77472be19f0bd1210959931c
Tamanna <tamannakhanum@hotmail.co.uk>
2024-03-18 15:56:52 +0000

    "Set up the very basic UI for the selectors of the game."

--- trivia/src/components/SelectQuiz.tsx
+++ trivia/src/components/SelectQuiz.tsx
@@ -5,6 +5,32 @@ const SelectQuiz = () => {
     <>
       <div className="background">
         <h1>Select Quiz</h1>
+        <form method="" className="form">
+          <div className="">
+
+            <label hidden htmlFor="amount"></label>
+            <select name="amount">
+              <option value="" disabled selected>No. of Questions</option>
+              <option value={10}>10</option>
+              <option value={20}>20</option>
+              <option value={30}>30</option>
+              <option value={40}>40</option>
+            </select>
+
+            <label hidden htmlFor="category"></label>
+            <select name="category">
+              <option value="" disabled selected>Select Category</option>
+            </select>
+
+            <label hidden htmlFor="difficulty"></label>
+            <select name="difficulty">
+              <option value="" disabled selected>Select Difficulty</option>
+              <option value="easy">Easy</option>
+              <option value="medium">Medium</option>
+              <option value="hard">Hard</option>
+            </select>
+          </div>
+        </form>
       </div>
     </>
   )
