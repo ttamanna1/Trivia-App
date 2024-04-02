84f0f603842306826c6aac5abef0a6c888c469f8
Tamanna <tamannakhanum@hotmail.co.uk>
2024-04-02 09:28:41 +0100

    "Set up a new session token when the user clicks on start game."

--- trivia/src/components/SelectQuiz.tsx
+++ trivia/src/components/SelectQuiz.tsx
@@ -1,17 +1,53 @@
-
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
 const SelectQuiz = () => {
+  const navigate = useNavigate()
+  const [sessionToken, setSessionToken] = useState<string>('')
+
+  const [formData, setFormData] = useState({
+    amount: 10,
+    // category: '', 
+    difficulty: 'easy'
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  };
+
+  const startNewGame = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      const response = await axios.get('https://opentdb.com/api_token.php?command=request')
+      const { token } = response.data
+      setSessionToken(token)
+
+      navigate('/trivia-game', {
+        state: {
+          formData: { ...formData },
+          sessionToken: token
+        }
+      })
+      
+    } catch (error) {
+      console.error('Error starting new game:', error)
+    }
+  }
+
+  
   return (
     <>
       <div className="background">
         <h1>Select Quiz</h1>
-        <form method="" className="form">
+        <form onSubmit={startNewGame} className="form">
           <div className="selections">
 
             <div className="select-container">
               <p>No. of Questions</p>
               <label hidden htmlFor="amount"></label>
-              <select name="amount" defaultValue={10}>
+              <select name="amount" value={formData.amount} onChange={handleChange}>
                 <option value={10}>10</option>
                 <option value={20}>20</option>
                 <option value={30}>30</option>
@@ -19,18 +55,18 @@ const SelectQuiz = () => {
               </select>
             </div>
 
-            <div className="select-container">
+            {/* <div className="select-container">
               <p>Select Category</p>
               <label hidden htmlFor="category"></label>
               <select name="category">
                 <option value="" disabled>Select Category</option>
               </select>
-            </div>
+            </div> */}
 
             <div className="select-container">
               <p>Select Difficulty</p>
               <label hidden htmlFor="difficulty"></label>
-              <select name="difficulty" defaultValue="easy">
+              <select name="difficulty" value={formData.difficulty} onChange={handleChange}>
                 <option value="easy">Easy</option>
                 <option value="medium">Medium</option>
                 <option value="hard">Hard</option>
@@ -40,7 +76,7 @@ const SelectQuiz = () => {
           </div>
 
           <div className="start-button">
-            <button>Start New Game!</button>
+            <button type="submit">Start New Game!</button>
           </div>
 
         </form>
